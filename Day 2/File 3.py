c29d0456a7d41721b2bf65f6a6121832b019f5bf
Tibin Abraham <tibinbm@gmail.com>
2024-09-12 19:58:32 +0530

    1

--- Day 2/File 3.py
+++ Day 2/File 3.py
@@ -1,1 +1,10 @@
-# In our monthly expense example, print monthly expenses 
+# In our monthly expense example, print month number and  expenses and then in the end print total expense
+
+exp = [2340,2500,2100,3100,2980]
+total = 0
+for i in range(len(exp)):
+    print("Month:",(i+1),'Expense:',exp[i])
+    total = total + exp[i]
+    #print("Total:", total)
+
+print("Total:",total)
